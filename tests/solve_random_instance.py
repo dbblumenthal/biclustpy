eaa3aeadeac6d6e17daf829243568412d17fb090
dblumenthal <davidbenjamin.blumenthal@unibz.it>
2019-06-25 22:48:43 +0200

    time limit in example

--- tests/solve_random_instance.py
+++ tests/solve_random_instance.py
@@ -10,9 +10,12 @@ parser = argparse.ArgumentParser(description="Computes biclusters for randomly g
 parser.add_argument("num_rows", type=int, help="Number of rows in the instance.")
 parser.add_argument("num_cols", type=int, help="Number of columns in the instance.")
 parser.add_argument("threshold", type=float, help="Edges whose weight is lower than the threshold are absent.")
+parser.add_argument("--time_limit", type=float, help="Time limit in seconds for optimizing the subproblems. Default = 60.")
 args = parser.parse_args()
 
 weights = np.random.rand(args.num_rows, args.num_cols)
 threshold = .99
 algorithm = bc.Algorithm()
+if (args.time_limit):
+    algorithm.ilp_time_limit = args.time_limit
 bi_clusters, obj_val, is_optimal = bc.compute_bi_clusters(weights, args.threshold, algorithm)
